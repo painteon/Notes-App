2e09f6fb0ee879e349f3de0753979b151e7d2cea
Bobby <painteon5@gmail.com>
2022-05-02 20:45:19 -0700

    create node server

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM, {createRoot} from 'react-dom/client';
 import App from './components/App.jsx';
 
+const express = require('express');
+const app = express();
+
+app.use(express.static('public');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log("Server running on " + PORT)
+})
+
 const root = createRoot(document.getElementById('root'))
 root.render(<App />);
